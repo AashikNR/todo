c6e856e10a62804f63c9c23bcd92eb51bca0ed53
AashikNR <ashiknizam77@gmail.com>
2020-06-04 12:46:42 +0530

    updated todo

--- todo1.js
+++ todo1.js
@@ -1,38 +1,52 @@
-
 var arr = [];
 
 function add(){
+    document.getElementById("todos").innerHTML = "";
     event.preventDefault();
     var new_value = document.getElementById('task').value;
-    arr.push(new_value);
-    console.log(arr); 
+    const newObj = {id:arr.length,task:new_value}
+    arr.push(newObj);
+    console.log(arr);
+    document.getElementById("add").style.display = "block";
+    document.getElementById("edit").style.display = "none";
     display();
 }
 
+
 function remove(){
-    var id = this.getAttribute('id');
+    document.getElementById("todos").innerHTML = "";
+    const id = this.getAttribute('id');
     arr.splice(id, 1);
-    console.log(arr); 
+    console.log(arr);
+    document.getElementById("add").style.display = "block";
+    document.getElementById("edit").style.display = "none";
     display();
 }
 
 function edit() {
-
+    document.getElementById("todos").innerHTML = "";
+    document.getElementById("edit").style.display = "block";
+    document.getElementById("add").style.display = "none";
+    const id = this.getAttribute('id');
+    var  edit_value = arr[id]["task"];
+    document.getElementById("task").value = edit_value;
+    arr.splice(id, 1);
+    display();
 
 }
 
 function display() {
-    var values = document.getElementById("todos");
+    const values = document.getElementById("todos");
     for(var i=0; i < arr.length; i++) {
-        values.innerHTML += "<li>"+arr[i]+"</li>";
+        values.innerHTML += "<li>"+arr[i]["task"]+"</li>";
 
-        var btn1 = document.createElement("BUTTON");
+        const btn1 = document.createElement("BUTTON");
         btn1.setAttribute("id" , i);
         btn1.setAttribute("class" , "edit");
         btn1.innerHTML = "EDIT";
         document.querySelector('#todos').appendChild(btn1);
 
-        var btn2 = document.createElement("BUTTON");
+        const btn2 = document.createElement("BUTTON");
         btn2.setAttribute("id" , i);
         btn2.setAttribute("class" , "remove");
         btn2.innerHTML = "DELETE";
@@ -40,10 +54,13 @@ function display() {
 
         document.getElementsByClassName('remove')[i].addEventListener('click', remove);
         document.getElementsByClassName('edit')[i].addEventListener('click', edit);
+
+        document.getElementById("task").value = "";
         
 
     };
 }
  
 document.getElementById('add').addEventListener('click', add);
+document.getElementById('edit').addEventListener('click', add);
 display();
